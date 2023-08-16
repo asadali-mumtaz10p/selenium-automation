17b9bcf2a85bc8abd1051ba02d98092ec0225c45
Asad Ali Mumtaz <asad.mumtaz@10pearls.com>
2023-08-16 13:16:15 +0500

    Task 2

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,18 +1,39 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.time.Instant;
+
 public class FirstTest {
 
     WebDriver driver;
     @Test
-    public void googleSearch() {
-//        System.setProperty("webdriver.http.factory", "jdk-http-client");
-//        System.setProperty("webdriver.chrome.driver","C:\\Users\\asad.mumtaz\\OneDrive - 10Pearls\\Documents\\Data BackUp\\Data BackUp\\Selenium Java\\chromedriver_win32");
-//
+    public void googleSearch() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://google.com");
+        driver.get("https://demoqa.com/automation-practice-form ");
+        WebElement FirstName = driver.findElement(By.className("mr-sm-2"));
+        WebElement LastName = driver.findElement(By.xpath("//*[@id=\"lastName\"]"));
+        WebElement email = driver.findElement(By.id("userEmail"));
+        WebElement Mobile = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/form/div[4]/div[2]/input"));
+        WebElement gender = driver.findElement(By.xpath("//div[@class='col-md-9 col-sm-12']//input[@value='Male']"));
+        Actions actions = new Actions(driver);
+        FirstName.sendKeys("Asad");
+        LastName.sendKeys("Ali");
+        email.sendKeys("asad123@abc.com");
+        actions.moveToElement(gender).click().build().perform();
+        Mobile.sendKeys("03123456789");
+
     }
-    }
+
+}
 
